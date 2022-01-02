08fb5f5d56be11bc9a142f41fe92b0f402a0f83f
markzum <markzum@yandex.ru>
2022-01-02 13:21:09 +0300

    finish administration page

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var app = express()
 var mongo = require("mongodb")
 
 
-var HOST = 'http://localhost'
+var HOST = 'http://localhost' //192.168.10.153
 var PORT = 3000
 var URL = HOST + ":" + PORT
 const MongoClient = mongo.MongoClient
 const mongoClient = new MongoClient("mongodb+srv://markzum:163schoolTOP1@cluster0.gm56v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority") //mongodb://localhost:27017/
 
 app.use('/static', express.static(__dirname + '/public')); //'static' is just url prefix
+app.use('/dist', express.static(__dirname + '/dist'));
 
 app.set('view engine', 'pug');
 
@@ -33,7 +34,7 @@ app.get('/', (req, res) => {
 
 
 
-//
+//administration
 app.get('/persons/administration', (req, res) => {
     mongoClient.connect(function(err, client){
         if (err) {return console.log(err)}
@@ -42,7 +43,8 @@ app.get('/persons/administration', (req, res) => {
     
         collection.find({class: "administration"}).toArray(function(err, results){
             if (err) {return console.log(err)}
-            res.render('administration', {host: URL, sectionTitle: "persons", persons: results});
+            var sorted_results = results.sort(sortPersonsArray);
+            res.render('administration', {host: URL, sectionTitle: "persons", persons: sorted_results});
             client.close();
         });
     });
@@ -50,12 +52,30 @@ app.get('/persons/administration', (req, res) => {
 
 
 
+//teachers
+app.get('/persons/teachers', (req, res) => {
+    mongoClient.connect(function(err, client){
+        if (err) {return console.log(err)}
+        const db = client.db("school");
+        const collection = db.collection("persons");
+    
+        collection.find({class: "teacher"}).toArray(function(err, results){
+            if (err) {return console.log(err)}
+            var sorted_results = results.sort(sortPersonsArray);
+            res.render('teachers', {host: URL, sectionTitle: "persons", persons: sorted_results});
+            client.close();
+        });
+    });
+});
+
+
+
+//person's page
 app.get('/persons/page*', (req, res) => {
     mongoClient.connect(function(err, client){
         if (err) {return console.log(err)}
         const db = client.db("school");
         const collection = db.collection("persons");
-        console.log(req.url.replace('/persons/page', ''))
         collection.find({"_id": mongo.ObjectId(req.url.replace('/persons/page', ''))}).toArray(function(err, results){
             if (err) {return console.log(err)}
             res.render('person-page', {host: URL, sectionTitle: "persons", person: results[0]});
@@ -68,11 +88,17 @@ app.get('/persons/page*', (req, res) => {
 
 // 404
 app.use(function(req, res, next) {
-    res.status(404).send('Sorry cant find that!');
+    res.status(404).render('error-404', {host: URL});
 });
 
 
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Example app listening at ${HOST}:${PORT}`)
 })
+
+
+
+function sortPersonsArray(x, y){
+    return x.name.localeCompare(y.name);
+}
