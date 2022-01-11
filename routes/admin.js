ee30f9687111b3b8a1af6b2378e118843cdadd74
markzum <markzum@yandex.ru>
2022-01-11 21:02:16 +0300

    made dark theme, documents storage, odod and social pedagogue pages

--- routes/admin.js
+++ routes/admin.js
@@ -270,6 +270,66 @@ router.get('/persons-storage/delete', checkAuth, (req, res) => {
 
 
 
+
+//documents-storage
+router.get('/documents-storage', checkAuth, (req, res) => {
+  var storage_dir = rootdir+"storage/documents-storage/"
+
+  fs.readdir(storage_dir, function(err, items) {
+    var files = []
+    if (items){
+      for (var i=0; i<items.length; i++) {
+          var file = {
+            "name": items[i],
+            "file": storage_dir + items[i],
+            "url": DOMAIN + "storage/documents-storage/" + items[i],
+          }
+          files.push(file)
+      }
+      res.render('admin/documents-storage', {host: DOMAIN, documents: files});
+    } else {
+      res.render('admin/documents-storage', {host: DOMAIN, documents: []});
+    }
+  })
+})
+
+
+
+router.post('/documents-storage/add', checkAuth, multer({dest:"storage/documents-storage/"}).single("document"), (req, res) => {
+  if (!req.file){
+    return res.redirect("/admin")
+  }
+
+  let photo = req.file;
+    
+  fs.rename(photo.path, photo.destination+photo.originalname, (err) => {
+    if (err) throw err;
+  });
+
+  res.redirect("/admin")
+
+})
+
+
+
+router.get('/documents-storage/delete', checkAuth, (req, res) => {
+  var filename = url.parse(req.url, true).query.filename
+
+  if (!filename){
+    return res.redirect("/admin")
+  }
+
+  fs.unlink(rootdir+"storage/documents-storage/"+filename, (err) => {
+    if (err) {return console.log(err)}
+  })
+
+  res.redirect("/admin")
+})
+
+
+
+
+
 // checkAuth
 function checkAuth(req, res, next) {
   // get cookies
--- routes/persons.js
+++ routes/persons.js
@@ -53,6 +53,27 @@ router.get('/teachers', (req, res) => {
 
 
 
+//teachers
+router.get('/teachers-odod', (req, res) => {
+    mongoClient.connect(function(err, client){
+        if (err) {return console.log(err)}
+        const db = client.db("school");
+        const collection = db.collection("persons");
+    
+        collection.find({class: "teacher_odod"}).toArray(function(err, results){
+            if (err) {return console.log(err)}
+            var sorted_results = results.sort(sortPersonsArray);
+            for (var pers of sorted_results) {
+                pers.img = pers.img.replace("%domain%", DOMAIN)
+            }
+            res.render('persons/teachers-odod', {host: DOMAIN, sectionTitle: "persons", persons: sorted_results});
+            client.close();
+        });
+    });
+});
+
+
+
 //person's page
 router.get('/person-page', (req, res) => {
     mongoClient.connect(function(err, client){
@@ -60,6 +81,7 @@ router.get('/person-page', (req, res) => {
         const db = client.db("school");
         const collection = db.collection("persons");
         var id = url.parse(req.url, true).query.id
+        if (id.length != 24){ return res.status(404).render('error-404', {host: DOMAIN}); }
         collection.find({"_id": mongo.ObjectId(id)}).toArray(function(err, results){
             if (err) {return console.log(err)}
             if (results[0]) {
@@ -72,6 +94,12 @@ router.get('/person-page', (req, res) => {
             client.close();
         });
     });
+});
+
+
+
+router.get('/social-pedagogue', (req, res) => {
+    res.render('persons/social-pedagogue', {host: DOMAIN, sectionTitle: "persons"}); 
 });
 
 
